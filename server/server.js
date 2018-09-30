e41dafe020832c634ae91519c67056796dad1982
Brandon <broffis@redventures.com>
2018-09-30 13:53:09 -0400

    Setup separate test and development envs

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -9,7 +11,7 @@ var {User} = require('./models/user');
 
 var app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
@@ -97,7 +99,7 @@ app.patch('/todos/:id', (req, res) => {
         }
 
         res.send({todo});
-        
+
     }).catch((e) => {
         res.status(400).send();
     })
